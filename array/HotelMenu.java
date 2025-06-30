9f07b04501a8a46e6f383ae33c3811eb13919077
unsatisfied17 <vilasnayak17@gmail.com>
2025-06-30 13:44:54 +0530

    adding array java files

---
+++ array/HotelMenu.java
@@ -0,0 +1,15 @@
+class HotelMenu{
+	static String southIndian[] = {"Idly", "Dosa", "Sambar", "Curry", "CoconutRice", "LemonRice", "CurdRice", "TomatoRice",};
+	static String northIndian[] = {"Dhokla", "Mishti", "Choolae", "Channa", "Daal", "Ghevar"};
+	static String chinees[] = {"Soup", "Raman", "Noodles", "Manchoorian", "FriedRice"};
+	
+	public static void main (String[] foods){
+	 
+	 System.out.println("The no of southIndian foods are "+ southIndian.length);
+	 System.out.println(southIndian[2] + " ");
+	
+	
+	}
+	
+
+}
