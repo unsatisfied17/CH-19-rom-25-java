630be2454949eca1e79d99dcab3e9df3de35b545
unsatisfied17 <vilasnayak17@gmail.com>
2025-06-30 13:20:01 +0530

    adding basic java files

---
+++ Netflix.java
@@ -0,0 +1,23 @@
+class Netflix {
+
+	static String kannadaMovies[] = {"KGF",  "KGF2",  "Gaalipata",  "Mungaru Mlalae",  "SapthaSagara",  "Andodn Etu Kaala",  "EbaniElida ELaeyalli",  "Love Moctail",  "Rajakumara",  "GandhaGudi"};
+	static String hindiMovies [] = {"Animal",  "3idots",  "Dil Baechara",  "Savan",  "Ashique",  "Kick",  "Sultan",  "Bombay",  "Border",  "Ghayal"};
+	static String tamilMovies[]   = {"Dragon", "Retro", "Kuberaa", "Raayan", "Amaran", "Maharaja", "Ayalaan", "Leo", "Jailer", "Master"};
+	static String teluguMovies[]  = {"Pushpa", "Pushpa2", "Sita Ramam", "Jersey", "Mayabazar", "Lucky Baskhar", "Arjun Reddy", "RRR", "Khushi", "Arya"};
+	static String malyalamMovies[] = {"Drishyam", "Premam", "Kumbalangi Nights", "Bangalore Days", "Jallikattu", "Charlie", "Uyare", "Nayattu", "1983", "Minnal Murali",};
+	static String englishMovies[] = {"The GodFather", "Seven Samurai", "Parasite", "Casablanca", "On The Waterfront", "Toy Story2", "Rear Window", "Modern Times", "Up", "Spotlight"};
+
+
+	public static void main(String[] movies){
+		System.out.println("The no of kannadaMovies added are " + kannadaMovies.length);
+		System.out.println(kannadaMovies[0] + "  " + kannadaMovies[1]);
+		System.out.println("The no of hindiMovies added are " + hindiMovies.length);
+		System.out.println(hindiMovies[3] + " ");
+		
+		
+		
+	}
+
+
+
+}
