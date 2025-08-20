99f640c583f55b6656ff574382eb0dfb0a83fc8f
unsatisfied17 <vilasnayak17@gmail.com>
2025-08-20 11:28:28 +0530

    committing basic java files

---
+++ amusement-park/src/com/xworkz/amusement/park/AmusementPark.java
@@ -0,0 +1,20 @@
+package com.xworkz.amusement.park;
+
+class AmusementPark {
+    String name;
+    String location;
+    String openingHours;
+    int numberOfRides;
+    double entryFee;
+
+    public void getAmusementPark(){
+
+        System.out.println("Amusement Park Name: " + name);
+        System.out.println("Location: " + location);
+        System.out.println("Opening Hours: " + openingHours);
+        System.out.println("Number of Rides: " + numberOfRides);
+        System.out.println("Entry Fee: $" + entryFee);
+
+
+    }
+}
